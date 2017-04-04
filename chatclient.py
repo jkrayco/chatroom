2f38e57f05b00eadaa2d7583eaebea105edd5002
jkrayco <sileruf@gmail.com>
2017-04-04 19:13:59 +0800

    Added base files

---
+++ chatclient.py
@@ -0,0 +1,36 @@
+import socket
+import sys
+import select
+import thread
+
+serv_addr = '127.0.0.1'
+port = 12314
+keep_alive = 1
+
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.connect((serv_addr, port))
+
+print("Connected to chat server.")
+print("IP Address:", serv_addr)
+print("Port:", port)
+
+def send():
+	while 1:
+		msg_out=sys.stdin.readline()		//unicode
+		msg_out=msg_out.encode('utf-8')		//utf-8
+		c.send(msg_out)
+		
+
+thread.start_new_thread(send, ())
+
+while 1:
+	try:
+		msg_in = c.recv(1024)
+		msg_in = msg_in.decode('utf-8')
+		print(msg_in)
+		if msg_in == '':
+			c.close()
+			break
+	except:
+		c.close()
+		break
